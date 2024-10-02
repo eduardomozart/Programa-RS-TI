6493d9498755ead088f3bbbb0a2adc9cea3d24ec
Renan Eberhardt <129560978+RenanEberhardt@users.noreply.github.com>
2024-10-02 19:26:45 -0300

    salvo

--- aula 1/script.js
+++ aula 1/script.js
@@ -10,12 +10,14 @@ $(document).ready(function() {
         cep = cep.replace("-", "");
         if(cep.length == 8){
             $("input[name=cep]").removeClass("is-invalid");
-            alert(cep);
-        } else if(cep.length == 0){
-            $("input[name=cep]").removeClass("is-invalid");
-        } else{
-            $("input[name=cep]").addClass("is-invalid");
-        }
+            //alert(cep);
+            $.ajax("https://viacep.com.br/ws/"+ cep +"/json")
+                .done(function(data){
+                    resposta = JSON.parse(data);
+                    alert(data);
+                });
+            
+        } 
         
     });
 }); 
