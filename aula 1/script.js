f53233b9f73731589174aa16549d00c437fd19ef
Renan Eberhardt <129560978+RenanEberhardt@users.noreply.github.com>
2024-10-02 21:35:27 -0300

    nfdfgh

--- aula 1/script.js
+++ aula 1/script.js
@@ -35,10 +35,42 @@ $(document).ready(function() {
             });
             
         } 
+    });
+    
+    const urlEstados = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+    
+    // Carregar os estados na inicialização
+    $.getJSON(urlEstados, function(data) {
+        data.sort(function(a, b) {
+            return a.nome.localeCompare(b.nome);
+        });
         
-        const urlEstados = 'https://'
+        data.forEach(function(estado) {
+            $('#estado').append(`<option value="${estado.sigla}">${estado.nome}</option>`);
+        });
     });
 
-    
+    $('#estado').on('change', function() {
+        let estadoId = $(this).val(); // Pega o ID do estado selecionado
 
+        if (estadoId) {
+            const urlCidades = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoId}/municipios`;
+
+            $.getJSON(urlCidades, function(data) {
+                $('#cidade').empty(); // Limpa o select de cidades
+                $('#cidade').append(`<option value="">Selecione a cidade</option>`); // Adiciona a opção padrão
+
+                data.sort(function(a, b) {
+                    return a.nome.localeCompare(b.nome);
+                });
+
+                data.forEach(function(cidade) {
+                    $('#cidade').append(`<option value="${cidade.id}">${cidade.nome}</option>`);
+                });
+            });
+        } else {
+            $('#cidade').empty(); // Limpa o select de cidades caso não haja estado selecionado
+            $('#cidade').append(`<option value="">Primeiro selecione o estado</option>`);
+        }
+    });
 }); 
