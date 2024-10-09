13a2f0285125e1feea5f58a9b0bf9fc2fede10ed
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-10-09 20:25:26 -0300

    Aula 3 - Coluna

--- aula3/script.js
+++ aula3/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(e) {
         for (var i = 0; i < data.results.length; i++) {
             var out = "<tr>";
             out += "<td scope='row'>" + (parseInt(i)+1) + "</td>";
-            // out += "<td><img src='" + data.results[i].picture.thumbnail + "' /></td>";
+            out += "<td><!-- <img src='" + data.results[i].picture.thumbnail + "' /> --></td>";
             out += "<td>" + data.results[i].login.username + "</td>";
             out += "</tr>"
 
