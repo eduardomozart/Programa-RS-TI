7076fc6305f6bee102d352234a6bf1e17f7fbd77
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-10-09 21:15:16 -0300

    Aula 3

--- aula3/javascript.js
+++ aula3/javascript.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
-    if(document.readyState == 'complete') {
-        $.getJSON("https://randomuser.me/api/?results=10&nat=br", function(data) {
-            for (var i = 0; i < data.results.length; i++) {
-                var out = "<tr>";
-                out += "<td scope='row'>" + (parseInt(i)+1) + "</td>";
-                out += "<td><!-- <img src='" + data.results[i].picture.thumbnail + "' /> --></td>";
-                out += "<td>" + data.results[i].login.username + "</td>";
-                out += "</tr>";
+    $.getJSON("https://randomuser.me/api/?results=10&nat=br", function(data) {
+        for (var i = 0; i < data.results.length; i++) {
+            var out = "<tr>";
+            out += "<td scope='row'>" + (i + 1) + "</td>";
+            out += "<td><img src='" + data.results[i].picture.thumbnail + "' alt='Foto' /></td>";
+            out += "<td>" + data.results[i].login.username + "</td>";
+            out += "<td>" + data.results[i].name.first + "</td>";
+            out += "<td>" + data.results[i].name.last + "</td>";
+            out += "<td>" + data.results[i].gender + "</td>";
+            out += "<td>" + data.results[i].email + "</td>";
+            out += "<td>" + data.results[i].phone + "</td>";
+            out += "<td>" + data.results[i].location.street.number + " " + data.results[i].location.street.name + "</td>";
+            out += "<td>" + data.results[i].location.city + "</td>";
+            out += "<td>" + data.results[i].location.state + "</td>";
+            out += "<td>" + data.results[i].location.country + "</td>";
+            out += "</tr>";
 
-                $("table tbody").append(out);
-            }
-        });
-    }
+            $("table tbody").append(out);
+        }
+    });
 });
